1dd1154cc80119a3b524cfc1adad6772803dde9d
Fábio <fabio.oliveira@poscontrole.com.br>
2025-01-13 22:20:48 -0300

    ✨ feat(api): add GET Task by ID endpoint
    - Add endpoint to retrieve a single task by its ID
    - Utilize the GetTaskByIdHandler function in the controller
    - Endpoint follows REST API standard by including ID in the URL path

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -11,6 +11,7 @@ func main() {
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("GET /tasks", controller.TasksHandler(task.GetTasks))
+	mux.HandleFunc("GET /tasks/{id}", controller.GetTaskByIdHandler(task.GetTaskById))
 	mux.HandleFunc("POST /tasks", controller.CreateTasksHandler(task.AddTask))
 	mux.HandleFunc("PUT /tasks/{id}", controller.UpdateTaskHandler(task.UpdateTask))
 	mux.HandleFunc("DELETE /tasks/{id}", controller.RemoveTaskHandler(task.RemoveTask))

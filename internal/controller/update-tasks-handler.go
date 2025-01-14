1c8d420ab0756b6c70b80a229d715197ae8ec658
Fábio <fabio.oliveira@poscontrole.com.br>
2025-01-13 21:49:48 -0300

    🐛 fix(controller): fix typo in UpdateTaskHandler comment
    - Change "Atualiza uma tarefa." to "Atualiza uma tarefa:"
    - Correct the punctuation to match formatting of other comments

---
+++ internal/controller/update-tasks-handler.go
@@ -0,0 +1,46 @@
+package controller
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"strconv"
+
+	"github.com/fabiosoliveira/task-api/internal/task"
+)
+
+// Atualiza uma tarefa.
+
+func UpdateTaskHandler(updateTask func(id int, name string) error) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		body, err := io.ReadAll(r.Body)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		defer r.Body.Close()
+
+		var task task.Task
+		err = json.Unmarshal(body, &task)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		// Atualiza a tarefa com o ID informado.
+		id, err := strconv.Atoi(r.PathValue("id"))
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		err = updateTask(id, task.Name)
+		if err != nil {
+			http.Error(w, fmt.Sprintf("task with ID %d not found", id), http.StatusNotFound)
+			return
+		}
+
+		w.WriteHeader(http.StatusOK)
+	}
+}

397c2bf827c382f2bb8c8cbbd83a7ba3ed61501b
Fábio <fabio.oliveira@poscontrole.com.br>
2025-01-13 21:49:37 -0300

    ✨ feat(task): add UpdateTask function
    - Allows updating a task's name by ID
    - Returns an error if task with given ID is not found
    
    Note: No changes to existing AddTask function.

--- internal/task/task.go
+++ internal/task/task.go
@@ -1,5 +1,7 @@
 package task
 
+import "fmt"
+
 type Task struct {
 	ID   int    `json:"id"`
 	Name string `json:"name"`
@@ -18,4 +20,15 @@ func AddTask(name string) Task {
 	task := Task{ID: lastId, Name: name}
 	tasks = append(tasks, task)
 	return task
+}
+
+func UpdateTask(id int, name string) error {
+	for i, t := range tasks {
+		if id == t.ID {
+			tasks[i].Name = name
+			return nil
+		}
+	}
+
+	return fmt.Errorf("task with ID %d not found", id)
 }

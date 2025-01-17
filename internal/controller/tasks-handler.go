c4fafd110c9f5aca5857fac46e107121e8e9a98b
Fábio <fabio.oliveira@poscontrole.com.br>
2025-01-17 20:24:13 -0300

    🐛 fix(controller): add read lock to avoid concurrent map read and write
    - Add read lock to prevent concurrent map read and write on tasks
    - Use a temporary variable to hold tasks while marshaling response

--- internal/controller/tasks-handler.go
+++ internal/controller/tasks-handler.go
@@ -13,7 +13,12 @@ import (
 func TasksHandler(getTasks func() []task.Task) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
-		response, err := json.Marshal(getTasks())
+
+		mu.RLock()
+		task := getTasks()
+		mu.RUnlock()
+
+		response, err := json.Marshal(task)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
